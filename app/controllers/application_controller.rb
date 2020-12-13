ae1d7ce5ea5dcda15ae2c1db677ee252c6936c5e
hayashi-3 <hayashi.gml@gmail.com>
2020-12-13 23:53:44 +0900

    error_function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionContarollerError; end
+
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :resucue403
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -9,6 +14,11 @@ class ApplicationController < ActionController::Base
     else
       "customer"
     end
+  end
+
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
   end
 
   private def rescue500(e)
--- app/controllers/staff/top_controller.rb
+++ app/controllers/staff/top_controller.rb
@@ -1,7 +1,6 @@
 class Staff::TopController < ApplicationController
 
   def index
-    raise
     render action: "index" 
   end
 
