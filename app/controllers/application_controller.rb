5e9e4026bfec1ac1751e756c1a756b070ae3055f
hayashi-3 <hayashi.gml@gmail.com>
2020-12-18 22:34:55 +0900

    [add]errors_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-  class Forbidden < ActionControllerError; end
-  class IpAddressRejected < ActionController::ActionContarollerError; end
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
 
   rescue_from StandardError, with: :rescue500
   rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :resucue403
+  rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -19,6 +20,10 @@ class ApplicationController < ActionController::Base
   private def rescue403(e)
     @exception = e
     render "errors/forbidden", status: 403
+  end
+
+  private def rescue404(e)
+    render "errors/not_found", status: 403
   end
 
   private def rescue500(e)
